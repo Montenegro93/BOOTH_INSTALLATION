08bfaf988656b15e3f43a5561fefcb526913dcd9
cegMo <46608529+cegMo@users.noreply.github.com>
2019-12-04 13:07:20 -0500

    fixed bug and started integration v2

--- BOOTH/deep-learning-face-detection/detect_faces_video.py
+++ BOOTH/deep-learning-face-detection/detect_faces_video.py
@@ -8,7 +8,7 @@ import argparse
 import imutils
 import time
 import cv2
-#importing osc python package
+
 from pythonosc.udp_client import SimpleUDPClient
 
 
@@ -17,14 +17,13 @@ port = 8888
 client = SimpleUDPClient(ip, port)  # Create client
 number_of_player = 0
 
-
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.8,
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
@@ -34,7 +33,7 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start() #select camera ID here
+vs = VideoStream(src=1).start()
 time.sleep(2.0)
 
 # loop over the frames from the video stream
@@ -48,13 +47,13 @@ while True:
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
 		(300, 300), (104.0, 177.0, 123.0))
-	
+ 
 	# pass the blob through the network and obtain the detections and
 	# predictions
 	net.setInput(blob)
 	detections = net.forward()
 
-		# loop over the detections
+	# loop over the detections
 	for i in range(0, detections.shape[2]):
 		# extract the confidence (i.e., probability) associated with the
 		# prediction
@@ -64,39 +63,23 @@ while True:
 		# greater than the minimum confidence
 		if confidence < args["confidence"]:
 			continue
-		
-		number_of_player = number_of_player + 1
+
 		# compute the (x, y)-coordinates of the bounding box for the
 		# object
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
-		#send face pos via osc
-		#posX = (startX - endX)*0.5 + startX
-		#posY = (startY - endY)*0.5 + startY
-		#print( posX , posY)
-		#pos = [posX , posY]
+ 
 		pos = [int(startX), int(endX), int(startY), int(endY)]
-		
-		adress = "/pos" + str(number_of_player)
-		
-		client.send_message(adress,pos)
-		#client.send_message(adressY,posY)
-		# draw the bounding box of the face along with the associated
-		# probability
-	#	text = "{:.2f}%".format(confidence * 100)
-		#y = startY - 10 if startY - 10 > 10 else startY + 10
-		#cv2.rectangle(frame, (startX, startY), (endX, endY),
-			#(0, 0, 255), 2)
-			
 
-		#cv2.putText(frame, text, (startX, y),
-			#cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		adress = "/pos" + str(number_of_player)
+		client.send_message(adress,pos)
+
+
 	# show the output frame
 	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
 	client.send_message('/numOfPlayer' ,  number_of_player)
 	number_of_player = 0
-	key = cv2.waitKey(1) & 0xFF
- 
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
